a45e8bfb49b9d4cae964a74e944f2ac66483dc2b
MinaminoYukas <minamino1997@outlook.jp>
2019-02-05 16:13:53 +0900

    最初

---
+++ LibSearchSys/LibSearchSys/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  LibSearchSys
+//
+//  Created by yuto on 2019/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://itunes.apple.com")!
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            // ここのエラーはクライアントサイドのエラー(ホストに接続できないなど)
+            if let error = error {
+                print("クライアントエラー: \(error.localizedDescription) \n")
+                return
+            }
+            
+            guard let data = data, let response = response as? HTTPURLResponse else {
+                print("no data or no response")
+                return
+            }
+            
+            if response.statusCode == 200 {
+                print(String(data: data, encoding: String.Encoding.utf8) ?? "")
+            } else {
+                // レスポンスのステータスコードが200でない場合などはサーバサイドエラー
+                print("サーバエラー ステータスコード: \(response.statusCode)\n")
+            }
+            
+        }
+        task.resume()
+    }
+
+
+}
+
